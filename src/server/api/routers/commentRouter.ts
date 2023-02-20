66f0c0d7cb9c776233b387091a2aa6a2bc14604a
ben-rieth <benrieth3@gmail.com>
2023-02-19 23:13:57 -0600

    Add update and delete routes

--- src/server/api/routers/commentRouter.ts
+++ src/server/api/routers/commentRouter.ts
@@ -2,44 +2,45 @@ import { createTRPCRouter, protectedProcedure } from "../trpc";
 import {z } from 'zod';
 
 const commentRouter = createTRPCRouter({
-    getAllComments: protectedProcedure
-        .input(z.object({
-            postId: z.string().cuid(),
-        }))
-        .query(({ input }) => {
-            return "get all comments"
-        }
-    ),
-
     leaveComment: protectedProcedure
         .input(z.object({
             postId: z.string().cuid(),
             commentText: z.string(),
-            commentAuthorId: z.string().cuid(),
         }))
-        .query(({ input }) => {
-            return "new comment"
+        .query( async ({ input, ctx }) => {
+            await ctx.prisma.comment.create({
+                data: {
+                    postId: input.postId,
+                    authorId: ctx.session.user.id,
+                    text: input.commentText
+                }
+            })
         }
     ),
 
     updateComment: protectedProcedure
         .input(z.object({
-            postId: z.string().cuid(),
             commentId: z.string().cuid(),
             newText: z.string(),
         }))
-        .query(({ input }) => {
-            return "updated comment";
+        .query(async ({ input, ctx }) => {
+            await ctx.prisma.comment.update({
+                where: { id: input.commentId },
+                data: { text: input.newText }
+            })
         }
     ),
 
     deleteComment: protectedProcedure
         .input(z.object({
-            postId: z.string().cuid(),
             commentId: z.string().cuid(),
         }))
-        .query(({ input }) => {
-            return "deleted comment";
+        .query(async ({ input, ctx }) => {
+            await ctx.prisma.comment.delete({
+                where: {
+                    id: input.commentId
+                }
+            })
         }
     ),
 });
