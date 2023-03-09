a2dfe964dbe9a81fb5c7fb714e600b7e3b8370d4
ben-rieth <benrieth3@gmail.com>
2023-03-09 17:59:06 -0600

    Create feed procedure

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -2,17 +2,23 @@ import type { GetServerSideProps, NextPage } from "next";
 import { type User, getServerSession } from "next-auth";
 import MainLayout from "../components/layouts/MainLayout";
 import CreatePost from "../components/posts/CreatePost";
+import Post from "../components/posts/Post";
 import { authOptions } from "../server/auth";
+import { api } from "../utils/api";
 type FeedPageProps = {
     user: User;
 }
 
 const FeedPage: NextPage<FeedPageProps>  = () => {
+    const { data: posts, isSuccess } = api.posts.getAllPosts.useQuery();
+
     return (
         <MainLayout title="Feed" description="Posts from the people that you follow!">
             <div className="flex flex-col items-center relative">
                 <p>Feed</p>
-                <CreatePost />
+                {isSuccess && posts.map(post => (
+                    <Post post={post} key={post.id} />
+                ))}
             </div>
         </MainLayout>
     );
--- src/server/api/routers/postRouter.ts
+++ src/server/api/routers/postRouter.ts
@@ -5,33 +5,25 @@ import { TRPCError } from "@trpc/server";
 const postRouter = createTRPCRouter({
     getAllPosts: protectedProcedure
         .query(({ ctx }) => {
-            return ctx.prisma.follows.findMany({
+            return ctx.prisma.post.findMany({
                 where: {
-                    followerId: ctx.session.user.id,
-                    status: 'accepted',
-                },
-                // get from the user's relationships
-                select: {
-                    following: {
-                        // users that they follow
-                        select: {
-                            id: true,
-                            firstName: true,
-                            lastName: true,
-                            image: true,
-                            // and their posts
-                            posts: {
-                                select: {
-                                    id: true,
-                                    text: true,
-                                    image: true,
-                                    createdAt: true,
-                                    comments: true
-                                }
+                    author: {
+                        followedBy: {
+                            some: {
+                                followerId: ctx.session.user.id,
+                                status: 'accepted',
                             }
                         }
                     }
+                },
+                include: {
+                    author: true,
+                    likedBy: true,
+                },
+                orderBy: {
+                    createdAt: 'desc',
                 }
+            
             })
         }),
 
