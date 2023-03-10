1d892526eda0eee4d6679900fcc4130224a46831
ben-rieth <benrieth3@gmail.com>
2023-03-09 21:38:04 -0600

    Add likes from feed page

--- src/pages/feed.tsx
+++ src/pages/feed.tsx
@@ -1,5 +1,7 @@
+import type { Likes } from "@prisma/client";
 import type { GetServerSideProps, NextPage } from "next";
 import { type User, getServerSession } from "next-auth";
+import toast from "react-hot-toast";
 import Button from "../components/general/Button";
 import MainLayout from "../components/layouts/MainLayout";
 import Post from "../components/posts/Post";
@@ -10,10 +12,55 @@ type FeedPageProps = {
     user: User;
 }
 
-const FeedPage: NextPage<FeedPageProps>  = () => {
+const FeedPage: NextPage<FeedPageProps>  = ({ user }) => {
     const { data, isSuccess, isLoading, fetchNextPage, hasNextPage } = api.posts.getAllPosts.useInfiniteQuery(
         { limit: 5 }, { getNextPageParam: (lastPage) => lastPage.nextCursor}
     );
+
+    const apiUtils = api.useContext();
+    const { mutate } = api.posts.toggleLike.useMutation({
+        onMutate: async (values) => {
+            await apiUtils.posts.getAllPosts.cancel();
+
+            apiUtils.posts.getAllPosts.setInfiniteData(
+                { limit: 5 },
+                prev => {
+                    if (!prev) 
+                        return {
+                            pages: [],
+                            pageParams: [],
+                        };
+                    
+                    return {
+                        ...prev, 
+                        pages: prev.pages.map(page => ({
+                            ...page,
+                            posts: page.posts.map((item) => {
+                                if (item.id !== values.postId) return item;
+
+                                return { 
+                                    ...item,
+                                    likedBy: [
+                                        ...item.likedBy,
+                                        {
+                                            postId: values.postId,
+                                            userId: user.id,
+                                        } as Likes
+                                    ]
+                                }
+                            })
+                        }))
+                    }
+                }
+            )
+        },
+
+        onError: () => toast.error("Could not like post"),
+
+        onSettled: async () => {
+            await apiUtils.posts.getAllPosts.invalidate({ limit: 5 });
+        }
+    });
 
     return (
         <MainLayout title="Feed" description="Posts from the people that you follow!">
@@ -23,7 +70,11 @@ const FeedPage: NextPage<FeedPageProps>  = () => {
                         {data.pages.map((group) => (
                             <>
                                 {group.posts.map(post => (
-                                    <Post post={post} key={post.id} containerClasses="w-full max-w-xl" />
+                                    <Post 
+                                        post={post} 
+                                        key={post.id} containerClasses="w-full max-w-xl" 
+                                        changeLike={(liked) => mutate({ postId: post.id, liked })}
+                                    />
                                 ))}
                             </>
                         ))}
@@ -49,7 +100,9 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     }
 
     return {
-        props: {}
+        props: {
+            user: session.user,
+        }
     }
 }
 
