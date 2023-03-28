92381e037ce2a99aaaeebcf2200ac4043835df5d
ben-rieth <benrieth3@gmail.com>
2023-03-28 16:08:05 -0500

    Add check to make sure post exists

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -29,41 +29,43 @@ const IndividualPostPage: NextPage<IndividualPostPageProps> = ({ user }) => {
 
     return (
         <MainLayout title="Benji Book" description="A user's post">
-            <div className="hidden md:block w-[90vw] mx-auto mt-5 max-w-[70rem]">
-                <Breadcrumbs>
-                    <BreadcrumbsLink title="Feed" href="/feed" />
-                    <BreadcrumbsLink title={`@${username}`} href={`/users/${post.authorId}`} last />
-                </Breadcrumbs>
-            </div>
             {isSuccess && (
-                <div className="relative flex flex-col items-center w-11/12 mx-auto mt-5 md:flex-row md:gap-8 md:justify-center md:items-baseline">
-                    <Post 
-                        post={post} 
-                        containerClasses="w-full max-w-xl flex-[3_3_0%] md:sticky md:top-5"
-                        currentUser={user}
-                    />
-                    
-                    <Tabs.Root className="flex flex-col w-full mx-auto px-5 flex-[2_2_0%] max-w-lg" defaultValue="comments">
-                        <Tabs.List className="shrink-0 flex gap-2 border-b border-black w-full px-2">
-                            <Tabs.Trigger asChild value="comments">
-                                <p className="text-sm lg:text-base px-5 py-2 rounded-t-lg w-fit text-center cursor-pointer  bg-white data-[state=active]:text-sky-500">
-                                    Comments ({post._count.comments})
-                                </p>
-                            </Tabs.Trigger>
-                            <Tabs.Trigger value="likes" asChild>
-                                <p className="text-sm lg:text-base px-5 py-2 rounded-t-lg w-fit text-center cursor-pointer bg-white data-[state=active]:text-sky-500">
-                                    Likes ({post._count.likedBy})
-                                </p>
-                            </Tabs.Trigger>
-                        </Tabs.List>
-                        <Tabs.Content value="comments">
-                            <CommentColumn postId={post.id} />
-                        </Tabs.Content>
-                        <Tabs.Content value="likes">
-                            <LikesColumn postId={post.id} />
-                        </Tabs.Content>
-                    </Tabs.Root>
-                </div>
+                <>
+                    <div className="hidden md:block w-[90vw] mx-auto mt-5 max-w-[70rem]">
+                        <Breadcrumbs>
+                            <BreadcrumbsLink title="Feed" href="/feed" />
+                            <BreadcrumbsLink title={`@${username}`} href={`/users/${post.authorId}`} last />
+                        </Breadcrumbs>
+                    </div>
+                    <div className="relative flex flex-col items-center w-11/12 mx-auto mt-5 md:flex-row md:gap-8 md:justify-center md:items-baseline">
+                        <Post 
+                            post={post} 
+                            containerClasses="w-full max-w-xl flex-[3_3_0%] md:sticky md:top-5"
+                            currentUser={user}
+                        />
+                        
+                        <Tabs.Root className="flex flex-col w-full mx-auto px-5 flex-[2_2_0%] max-w-lg" defaultValue="comments">
+                            <Tabs.List className="shrink-0 flex gap-2 border-b border-black w-full px-2">
+                                <Tabs.Trigger asChild value="comments">
+                                    <p className="text-sm lg:text-base px-5 py-2 rounded-t-lg w-fit text-center cursor-pointer  bg-white data-[state=active]:text-sky-500">
+                                        Comments ({post._count.comments})
+                                    </p>
+                                </Tabs.Trigger>
+                                <Tabs.Trigger value="likes" asChild>
+                                    <p className="text-sm lg:text-base px-5 py-2 rounded-t-lg w-fit text-center cursor-pointer bg-white data-[state=active]:text-sky-500">
+                                        Likes ({post._count.likedBy})
+                                    </p>
+                                </Tabs.Trigger>
+                            </Tabs.List>
+                            <Tabs.Content value="comments">
+                                <CommentColumn postId={post.id} />
+                            </Tabs.Content>
+                            <Tabs.Content value="likes">
+                                <LikesColumn postId={post.id} />
+                            </Tabs.Content>
+                        </Tabs.Root>
+                    </div>
+                </>
             )}
             {isLoading && (
                 <Loader text="Loading post" />
