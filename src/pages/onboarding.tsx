2a7f401d29ae27fba36f1acf4d0a3be3979250fa
ben-rieth <benrieth3@gmail.com>
2023-03-28 11:38:21 -0500

    Users can return to sign in page

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -1,15 +1,29 @@
 import type { GetServerSideProps, NextPage } from "next";
 import { getServerSession } from "next-auth";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
 import AboutYouForm from "../components/auth/AboutYouForm";
+import Button from "../components/general/Button";
 import AuthLayout from "../components/layouts/AuthLayout";
 import Logo from "../components/logo/Logo";
 import { authOptions } from "../server/auth";
 
 const OnboardingPage: NextPage = () => {
+
+    const router = useRouter();
+
+    const logOut = async () => {
+        const data = await signOut({ redirect: false, callbackUrl: '/' });
+        await router.push(data.url)
+    }
+    
     return (
         <AuthLayout description="Tell us about yourself so we can setup your Benji Book account!">
             <Logo />
             <AboutYouForm />
+            <Button variant="minimal" onClick={logOut}>
+                Return to Sign In Page
+            </Button>
         </AuthLayout>
     );
 };
